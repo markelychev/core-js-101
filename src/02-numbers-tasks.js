01425cd4ec68fa12e7c4f3cfdfb514cbc460b16d
A.Markelychev <a.markelychev@gmail.com>
2020-12-12 23:22:05 +0300

    feat: numbers-tasks finish

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -203,8 +203,13 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(num) {
+  if (num <= 1) return false;
+  const sqrt = Math.sqrt(num);
+  for (let i = 2; i <= sqrt; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
 }
 
 /**
@@ -222,8 +227,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number.isNaN(+value) ? def : +value;
 }
 
 module.exports = {
